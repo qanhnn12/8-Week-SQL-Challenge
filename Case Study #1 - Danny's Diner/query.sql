848d72c8484fd3cf0cbb71979060c7eb0b913543
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-05 08:50:13 +0700

    Update query.sql

--- Case Study #1 - Danny's Diner/query.sql
+++ Case Study #1 - Danny's Diner/query.sql
@@ -72,13 +72,3 @@ FROM dbo.sales;
 --CASE STUDY QUESTIONS--
 ------------------------
 
---1. What is the total amount each customer spent at the restaurant?
-
-SELECT 
-	s.customer_id, 
-	SUM(m.price) AS total_pay
-FROM sales s
-JOIN menu m 
-	ON s.product_id = m.product_id
-GROUP BY s.customer_id
-
