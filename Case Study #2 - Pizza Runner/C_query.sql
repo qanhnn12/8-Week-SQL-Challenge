de60c1dad0bd188c6c4dd6c39a91c03c8be3e6af
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-10 11:02:41 +0700

    Update C_query.sql

--- Case Study #2 - Pizza Runner/C_query.sql
+++ Case Study #2 - Pizza Runner/C_query.sql
@@ -1,22 +1,22 @@
 -- 1. What are the standard ingredients for each pizza?
 
 SELECT 
-	p.pizza_name,
-	STRING_AGG(t.topping_name, ', ') AS ingredients
+  p.pizza_name,
+  STRING_AGG(t.topping_name, ', ') AS ingredients
 FROM #toppingsBreak t
 JOIN pizza_names p 
-	ON t.pizza_id = p.pizza_id
+  ON t.pizza_id = p.pizza_id
 GROUP BY p.pizza_name
 
 
 --2. What was the most commonly added extra?
 
 SELECT 
-	p.topping_name,
-	COUNT(*) AS extra_count
+  p.topping_name,
+  COUNT(*) AS extra_count
 FROM #extrasBreak e
 JOIN pizza_toppings p
-	ON e.extra_id = p.topping_id
+  ON e.extra_id = p.topping_id
 GROUP BY p.topping_name
 ORDER BY COUNT(*) DESC;
 
@@ -24,11 +24,11 @@ ORDER BY COUNT(*) DESC;
 --3. What was the most common exclusion?
 
 SELECT 
-	p.topping_name,
-	COUNT(*) AS exclusion_count
+  p.topping_name,
+  COUNT(*) AS exclusion_count
 FROM #exclusionsBreak e
 JOIN pizza_toppings p
-	ON e.exclusion_id = p.topping_id
+  ON e.exclusion_id = p.topping_id
 GROUP BY p.topping_name
 ORDER BY COUNT(*) DESC;
 
@@ -40,46 +40,46 @@ ORDER BY COUNT(*) DESC;
 --Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
 
 WITH cteExtras AS (
-	SELECT 
-		e.record_id,
-		'Extra ' + STRING_AGG(t.topping_name, ', ') AS record_options
-	FROM #extrasBreak e
-	JOIN pizza_toppings t
-	ON e.extra_id = t.topping_id
-	GROUP BY e.record_id
+  SELECT 
+    e.record_id,
+    'Extra ' + STRING_AGG(t.topping_name, ', ') AS record_options
+  FROM #extrasBreak e
+  JOIN pizza_toppings t
+    ON e.extra_id = t.topping_id
+  GROUP BY e.record_id
 ), 
 cteExclusions AS (
-	SELECT 
-		e.record_id,
-		'Exclusion ' + STRING_AGG(t.topping_name, ', ') AS record_options
-	FROM #exclusionsBreak e
-	JOIN pizza_toppings t
-	ON e.exclusion_id = t.topping_id
-	GROUP BY e.record_id
+  SELECT 
+    e.record_id,
+    'Exclusion ' + STRING_AGG(t.topping_name, ', ') AS record_options
+  FROM #exclusionsBreak e
+  JOIN pizza_toppings t
+    ON e.exclusion_id = t.topping_id
+  GROUP BY e.record_id
 ), 
 cteUnion AS (
-	SELECT * FROM cteExtras
-	UNION
-	SELECT * FROM cteExclusions
+  SELECT * FROM cteExtras
+  UNION
+  SELECT * FROM cteExclusions
 )
 
 SELECT 
-	c.record_id,
-	c.order_id,
-	c.customer_id,
-	c.pizza_id,
-	c.order_time,
-	CONCAT_WS(' - ', p.pizza_name, STRING_AGG(u.record_options, ' - ')) AS pizza_info
+  c.record_id,
+  c.order_id,
+  c.customer_id,
+  c.pizza_id,
+  c.order_time,
+  CONCAT_WS(' - ', p.pizza_name, STRING_AGG(u.record_options, ' - ')) AS pizza_info
 FROM #customer_orders_temp c
 LEFT JOIN cteUnion u
-	ON c.record_id = u.record_id
+  ON c.record_id = u.record_id
 JOIN pizza_names p
-	ON c.pizza_id = p.pizza_id
+  ON c.pizza_id = p.pizza_id
 GROUP BY
-	c.record_id, 
-	c.order_id,
-	c.customer_id,
-	c.pizza_id,
-	c.order_time,
-	p.pizza_name
+  c.record_id, 
+  c.order_id,
+  c.customer_id,
+  c.pizza_id,
+  c.order_time,
+  p.pizza_name
 ORDER BY record_id;
