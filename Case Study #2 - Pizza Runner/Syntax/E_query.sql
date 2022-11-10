e63444c4250fb39feb6e09c0b2e4c9592d89e857
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-10 18:52:13 +0700

    Update E_query.sql

--- Case Study #2 - Pizza Runner/Syntax/E_query.sql
+++ Case Study #2 - Pizza Runner/Syntax/E_query.sql
@@ -1,6 +1,6 @@
-------------------
---Bonus Question--
-------------------
+-------------------
+--Bonus Questions--
+-------------------
 
 --If Danny wants to expand his range of pizzas - how would this impact the existing data design? 
 --Write an INSERT statement to demonstrate what would happen if a new Supreme pizza with 
