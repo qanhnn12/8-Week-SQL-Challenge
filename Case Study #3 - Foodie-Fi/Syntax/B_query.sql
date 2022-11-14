9ba1b20b79c5f092f8c934dbe4f98ca4a8799c9a
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-14 18:05:37 +0700

    Create B_query.sql

---
+++ Case Study #3 - Foodie-Fi/Syntax/B_query.sql
@@ -0,0 +1,9 @@
+------------------------------
+--B. Data Analysis Questions--
+------------------------------
+
+--1. How many customers has Foodie-Fi ever had?
+
+SELECT COUNT(DISTINCT customer_id)
+FROM subscriptions;
+
