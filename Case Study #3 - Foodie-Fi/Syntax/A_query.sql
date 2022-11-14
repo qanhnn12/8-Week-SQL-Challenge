148bdc99793b930ff7d18337fe1469d7d9b41a1f
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-14 18:02:49 +0700

    Update A_query.sql

--- Case Study #3 - Foodie-Fi/Syntax/A_query.sql
+++ Case Study #3 - Foodie-Fi/Syntax/A_query.sql
@@ -1,3 +1,10 @@
+-----------------------
+--A. Customer Journey--
+-----------------------
+
+--Based off the 8 sample customers provided in the sample from the subscriptions table, 
+--write a brief description about each customer’s onboarding journey.
+
 SELECT 
 	s.*,
 	p.plan_name,
