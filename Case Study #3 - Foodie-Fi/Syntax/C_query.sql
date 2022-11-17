05ee73c30a02ac7ea653bb08bb814f5edd3f1a66
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-17 16:46:23 +0700

    Update C_query.sql

--- Case Study #3 - Foodie-Fi/Syntax/C_query.sql
+++ Case Study #3 - Foodie-Fi/Syntax/C_query.sql
@@ -38,8 +38,9 @@ WITH dateRecursion AS (
       --if a customer still used the current plan, next_date = '2020-12-31'
       WHEN LEAD(s.start_date) OVER(PARTITION BY s.customer_id ORDER BY s.start_date) IS NULL THEN '2020-12-31'
       -- if a customer changed the plan, next_date = (month difference between start_date and changing date) + start_date
-      ELSE DATEADD(MONTH, DATEDIFF(MONTH, start_date, LEAD(s.start_date) OVER(PARTITION BY s.customer_id ORDER BY s.start_date)),
-		 start_date) END AS next_date,
+      ELSE DATEADD(MONTH, 
+		   DATEDIFF(MONTH, start_date, LEAD(s.start_date) OVER(PARTITION BY s.customer_id ORDER BY s.start_date)),
+		   start_date) END AS next_date,
     p.price AS amount
   FROM subscriptions s
   JOIN plans p ON s.plan_id = p.plan_id
@@ -51,17 +52,17 @@ WITH dateRecursion AS (
   UNION ALL
 
   SELECT 
-  customer_id,
-  plan_id,
-  plan_name,
-  --increment payment_date by monthly
-  DATEADD(MONTH, 1, payment_date) AS payment_date,
-  next_date,
-		amount
-	FROM dateRecursion
+    customer_id,
+    plan_id,
+    plan_name,
+    --increment payment_date by monthly
+    DATEADD(MONTH, 1, payment_date) AS payment_date,
+    next_date,
+    amount
+  FROM dateRecursion
   --stop incrementing when payment_date = next_date
-	WHERE DATEADD(MONTH, 1, payment_date) <= next_date
-	AND plan_name != 'pro annual'
+  WHERE DATEADD(MONTH, 1, payment_date) <= next_date
+    AND plan_name != 'pro annual'
 )
 
 --Insert into table [payments]
