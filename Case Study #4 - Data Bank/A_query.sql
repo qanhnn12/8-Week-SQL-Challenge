ee0c0c71b56c693ac8c430295b07bda150f59dda
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-19 21:57:03 +0700

    Create A_query.sql

---
+++ Case Study #4 - Data Bank/A_query.sql
@@ -0,0 +1,18 @@
+---------------------------------
+--A. Customer Nodes Exploration--
+---------------------------------
+
+--1. How many unique nodes are there on the Data Bank system?
+SELECT COUNT(DISTINCT node_id) AS unique_nodes
+FROM customer_nodes;
+
+--2. What is the number of nodes per region?
+SELECT 
+	r.region_id,
+	r.region_name,
+	COUNT(n.node_id) AS nodes
+FROM customer_nodes n
+JOIN regions r
+ON n.region_id = r.region_id
+GROUP BY r.region_id, r.region_name
+ORDER BY r.region_id;
