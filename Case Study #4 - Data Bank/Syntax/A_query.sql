47ebb0a4e4fd2a45aa3cb4267cd707658e38dd6e
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-19 21:57:38 +0700

    Update and rename Case Study #4 - Data Bank/A_query.sql to Case Study #4 - Data Bank/Syntax/A_query.sql

--- Case Study #4 - Data Bank/Syntax/A_query.sql
+++ Case Study #4 - Data Bank/Syntax/A_query.sql
@@ -3,16 +3,19 @@
 ---------------------------------
 
 --1. How many unique nodes are there on the Data Bank system?
+
 SELECT COUNT(DISTINCT node_id) AS unique_nodes
 FROM customer_nodes;
 
+
 --2. What is the number of nodes per region?
+
 SELECT 
-	r.region_id,
-	r.region_name,
-	COUNT(n.node_id) AS nodes
+  r.region_id,
+  r.region_name,
+  COUNT(n.node_id) AS nodes
 FROM customer_nodes n
 JOIN regions r
-ON n.region_id = r.region_id
+  ON n.region_id = r.region_id
 GROUP BY r.region_id, r.region_name
 ORDER BY r.region_id;
