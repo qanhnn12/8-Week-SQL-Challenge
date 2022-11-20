961203eba71a93a415b5cd84029c35d211bf8563
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-20 14:38:15 +0700

    Update B_query.sql

--- Case Study #4 - Data Bank/Syntax/B_query.sql
+++ Case Study #4 - Data Bank/Syntax/B_query.sql
@@ -34,20 +34,20 @@ FROM customerDeposit;
 --3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
 
 WITH cte_transaction AS (
-	SELECT 
-		customer_id,
-		MONTH(txn_date) AS months,
-		SUM(CASE WHEN txn_type = 'deposit' THEN 1 ELSE 0 END) AS deposit_count,
-		SUM(CASE WHEN txn_type = 'purchase' THEN 1 ELSE 0 END) AS purchase_count,
-		SUM(CASE WHEN txn_type = 'withdrawal' THEN 1 ELSE 0 END) AS withdrawal_count
-	FROM customer_transactions
-	GROUP BY customer_id, MONTH(txn_date)
+  SELECT 
+    customer_id,
+    MONTH(txn_date) AS months,
+    SUM(CASE WHEN txn_type = 'deposit' THEN 1 ELSE 0 END) AS deposit_count,
+    SUM(CASE WHEN txn_type = 'purchase' THEN 1 ELSE 0 END) AS purchase_count,
+    SUM(CASE WHEN txn_type = 'withdrawal' THEN 1 ELSE 0 END) AS withdrawal_count
+  FROM customer_transactions
+  GROUP BY customer_id, MONTH(txn_date)
 )
 
 SELECT 
-	months,
-	COUNT(customer_id) AS customer_count
+  months,
+  COUNT(customer_id) AS customer_count
 FROM cte_transaction
 WHERE deposit_count > 1
-AND (purchase_count = 1 OR withdrawal_count = 1)
+  AND (purchase_count = 1 OR withdrawal_count = 1)
 GROUP BY months;
