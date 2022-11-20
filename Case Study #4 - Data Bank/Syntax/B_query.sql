22eb77f1d8fe8a7dfaaf7e001a143ed646094131
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-20 14:28:24 +0700

    Update B_query.sql

--- Case Study #4 - Data Bank/Syntax/B_query.sql
+++ Case Study #4 - Data Bank/Syntax/B_query.sql
@@ -5,9 +5,29 @@
 --1. What is the unique count and total amount for each transaction type?
 
 SELECT 
-	txn_type,
-	COUNT(*) AS unique_count,
-	SUM(txn_amount) AS total_amount
+  txn_type,
+  COUNT(*) AS unique_count,
+  SUM(txn_amount) AS total_amount
 FROM customer_transactions
 GROUP BY txn_type;
+
+
+--2. What is the average total historical deposit counts and amounts for all customers?
+
+WITH customerDeposit AS (
+  SELECT 
+    customer_id,
+    txn_type,
+    COUNT(*) AS dep_count,
+    SUM(txn_amount) AS dep_amount
+  FROM customer_transactions
+  WHERE txn_type = 'deposit'
+  GROUP BY customer_id, txn_type
+)
+
+SELECT
+  AVG(dep_count) AS avg_dep_count,
+  AVG(dep_amount) AS avg_dep_amount
+FROM customerDeposit;
+
 
