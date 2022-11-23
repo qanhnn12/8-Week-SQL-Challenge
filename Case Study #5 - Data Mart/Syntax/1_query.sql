767c85fd523f863d4da997e7a4c94cb302b6c5bf
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-23 22:17:58 +0700

    Update 1_query.sql

--- Case Study #5 - Data Mart/Syntax/1_query.sql
+++ Case Study #5 - Data Mart/Syntax/1_query.sql
@@ -3,19 +3,19 @@
 ---------------------------
 
 SELECT
-	CONVERT(date, week_date, 3) AS week_date,
-	DATEPART(week, CONVERT(date, week_date, 3)) AS week_number,
-	DATEPART(month, CONVERT(date, week_date, 3)) AS month_number,
-	DATEPART(year, CONVERT(date, week_date, 3)) AS calendar_year,
-	CASE 
-		WHEN RIGHT(segment, 1) = '1' THEN 'Young Adults'
-		WHEN RIGHT(segment, 1) = '2' THEN 'Middle Aged'
-		WHEN RIGHT(segment, 1) IN ('3', '4') THEN 'Retirees'
-		ELSE 'unknown' END AS age_band,
-	CASE 
-		WHEN LEFT(segment, 1) = 'C' THEN 'Couples'
-		WHEN LEFT(segment, 1) = 'F' THEN 'Families'
-		ELSE 'unknown' END AS demographic,
-	ROUND(CAST(sales AS FLOAT)/transactions, 2) AS avg_transaction
+  CONVERT(date, week_date, 3) AS week_date,
+  DATEPART(week, CONVERT(date, week_date, 3)) AS week_number,
+  DATEPART(month, CONVERT(date, week_date, 3)) AS month_number,
+  DATEPART(year, CONVERT(date, week_date, 3)) AS calendar_year,
+  CASE 
+    WHEN RIGHT(segment, 1) = '1' THEN 'Young Adults'
+    WHEN RIGHT(segment, 1) = '2' THEN 'Middle Aged'
+    WHEN RIGHT(segment, 1) IN ('3', '4') THEN 'Retirees'
+    ELSE 'unknown' END AS age_band,
+    CASE 
+      WHEN LEFT(segment, 1) = 'C' THEN 'Couples'
+      WHEN LEFT(segment, 1) = 'F' THEN 'Families'
+      ELSE 'unknown' END AS demographic,
+    ROUND(CAST(sales AS FLOAT)/transactions, 2) AS avg_transaction
 INTO clean_weekly_sales
 FROM weekly_sales;
