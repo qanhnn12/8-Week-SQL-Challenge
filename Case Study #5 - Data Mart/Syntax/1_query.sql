64cfea5b38c25ad20f919d6b8cf0367f2cfcdcad
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-23 22:22:41 +0700

    Update 1_query.sql

--- Case Study #5 - Data Mart/Syntax/1_query.sql
+++ Case Study #5 - Data Mart/Syntax/1_query.sql
@@ -7,6 +7,10 @@ SELECT
   DATEPART(week, CONVERT(date, week_date, 3)) AS week_number,
   DATEPART(month, CONVERT(date, week_date, 3)) AS month_number,
   DATEPART(year, CONVERT(date, week_date, 3)) AS calendar_year,
+  region,
+  platform,
+  segment,
+  customer_type,
   CASE 
     WHEN RIGHT(segment, 1) = '1' THEN 'Young Adults'
     WHEN RIGHT(segment, 1) = '2' THEN 'Middle Aged'
@@ -16,7 +20,8 @@ SELECT
       WHEN LEFT(segment, 1) = 'C' THEN 'Couples'
       WHEN LEFT(segment, 1) = 'F' THEN 'Families'
       ELSE 'unknown' END AS demographic,
-    ROUND(CAST(sales AS FLOAT)/transactions, 2) AS avg_transaction,
-  sales
+  transactions,
+  sales,
+  ROUND(CAST(sales AS FLOAT)/transactions, 2) AS avg_transaction
 INTO clean_weekly_sales
 FROM weekly_sales;
