b5a1e2394d28fca1708f5bfa0f2019c167a48e8d
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-23 22:18:15 +0700

    Update 1_query.sql

--- Case Study #5 - Data Mart/Syntax/1_query.sql
+++ Case Study #5 - Data Mart/Syntax/1_query.sql
@@ -12,7 +12,7 @@ SELECT
     WHEN RIGHT(segment, 1) = '2' THEN 'Middle Aged'
     WHEN RIGHT(segment, 1) IN ('3', '4') THEN 'Retirees'
     ELSE 'unknown' END AS age_band,
-    CASE 
+  CASE 
       WHEN LEFT(segment, 1) = 'C' THEN 'Couples'
       WHEN LEFT(segment, 1) = 'F' THEN 'Families'
       ELSE 'unknown' END AS demographic,
