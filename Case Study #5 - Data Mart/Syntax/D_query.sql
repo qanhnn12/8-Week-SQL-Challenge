47272cb9b1c58fe2416cf314658396f97e0cc7d4
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-25 13:45:35 +0700

    Update and rename 4_query.sql to D_query.sql

--- Case Study #5 - Data Mart/Syntax/D_query.sql
+++ Case Study #5 - Data Mart/Syntax/D_query.sql
@@ -1,5 +1,5 @@
 ---------------------
---4. Bonus Question--
+--D. Bonus Question--
 ---------------------
 
 -- Which areas of the business have the highest negative impact in sales metrics performance in 2020 for the 12 week before and after period?
