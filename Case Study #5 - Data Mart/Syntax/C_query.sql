eb498a6769417b9364d972b05e209483cb92147a
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-25 13:46:17 +0700

    Update C_query.sql

--- Case Study #5 - Data Mart/Syntax/C_query.sql
+++ Case Study #5 - Data Mart/Syntax/C_query.sql
@@ -1,5 +1,5 @@
 ------------------------------
---3. Before & After Analysis--
+--C. Before & After Analysis--
 ------------------------------
 
 --1. What is the total sales for the 4 weeks before and after 2020-06-15? What is the growth or reduction rate in actual values and percentage of sales?
