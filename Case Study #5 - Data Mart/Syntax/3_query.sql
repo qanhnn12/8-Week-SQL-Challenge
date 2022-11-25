7a2f7507056f64b78715d36ac2b66ef3cbcd832b
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-25 10:48:06 +0700

    Update 3_query.sql

--- Case Study #5 - Data Mart/Syntax/3_query.sql
+++ Case Study #5 - Data Mart/Syntax/3_query.sql
@@ -30,3 +30,34 @@ SELECT *,
   CAST(100.0 * (after_changes-before_changes)/before_changes AS decimal(5,2)) AS pct_change
 FROM salesChanges;
 
+
+--2. What about the entire 12 weeks before and after?
+
+--Find the week_number of '2020-06-15' (@weekNum=25)
+DECLARE @weekNum int = (
+	SELECT DISTINCT week_number
+	FROM clean_weekly_sales
+	WHERE week_date = '2020-06-15')
+
+--Find the total sales of 12 weeks before and after @weekNum
+WITH packageChanges AS (
+	SELECT 
+		week_number,
+		SUM(sales) AS total_sales
+	FROM clean_weekly_sales
+	WHERE week_number BETWEEN @weekNum-12 AND @weekNum+11
+	AND calendar_year = 2020
+	GROUP BY week_number)
+,
+--Sepatate sales before and after @weekNum
+salesChanges AS (
+	SELECT
+		SUM(CASE WHEN week_number BETWEEN @weekNum-12 AND @weekNum-1 THEN total_sales END) AS before_changes,
+		SUM(CASE WHEN week_number BETWEEN @weekNum AND @weekNum+11 THEN total_sales END) AS after_changes
+	FROM packageChanges
+)
+
+SELECT *,
+	CAST(100.0 * (after_changes-before_changes)/before_changes AS decimal(5,2)) AS pct_change
+FROM salesChanges;
+
