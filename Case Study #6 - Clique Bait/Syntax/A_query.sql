7558271dc34b7c93309a2a3d9d3573f4d9c57a0e
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-11-28 16:50:09 +0700

    Update A_query.sql

--- Case Study #6 - Clique Bait/Syntax/A_query.sql
+++ Case Study #6 - Clique Bait/Syntax/A_query.sql
@@ -34,11 +34,49 @@ ORDER BY months;
 
 SELECT 
   e.event_type,
-  i.event_name,
+  ei.event_name,
   COUNT(*) AS event_count
 FROM events e
-JOIN event_identifier i
-  ON e.event_type = i.event_type
-GROUP BY e.event_type, i.event_name
+JOIN event_identifier ei
+  ON e.event_type = ei.event_type
+GROUP BY e.event_type, ei.event_name
 ORDER BY e.event_type;
+
+
+--5. What is the percentage of visits which have a purchase event?
+
+SELECT 
+	CAST(100.0*COUNT(e.visit_id) 
+  --select distinct here because each visit can have multiple events
+	/ (SELECT COUNT(DISTINCT visit_id) FROM events) AS decimal(10,2)) AS purchase_pct
+FROM events e
+JOIN event_identifier ei
+ON e.event_type = ei.event_type
+WHERE ei.event_name = 'Purchase';
+
+
+--6. What is the percentage of visits which view the checkout page but do not have a purchase event?
+
+WITH view_checkout AS (
+	SELECT COUNT(visit_id) AS cnt 
+	FROM events
+	WHERE event_type = 1
+	AND page_id = 12
+),
+purchase_list AS (
+	SELECT visit_id 
+	FROM events
+	WHERE event_type = 3)
+
+SELECT 
+	CAST(100.0*COUNT(visit_id)
+	/ (SELECT cnt FROM view_checkout) AS decimal(10,2)) AS pct_view_checkout_not_purchase
+FROM events
+--view the checkout page
+WHERE event_type = 1 AND page_id = 12
+-- but not purchase
+AND visit_id NOT IN (SELECT visit_id FROM purchase_list)
+
+
+
 
