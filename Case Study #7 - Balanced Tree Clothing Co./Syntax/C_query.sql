366693e7dfd7168cb4bf75ec390728dcc42bc736
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-02 16:33:42 +0700

    Update C_query.sql

--- Case Study #7 - Balanced Tree Clothing Co./Syntax/C_query.sql
+++ Case Study #7 - Balanced Tree Clothing Co./Syntax/C_query.sql
@@ -47,3 +47,38 @@ SELECT
   total_quantity
 FROM segment_sales
 WHERE rnk = 1;
+
+
+--4. What is the total quantity, revenue and discount for each category?
+
+SELECT 
+	pd.category_name,
+	SUM(s.qty) AS total_quantity,
+	SUM(s.qty * s.price) AS total_revenue,
+	SUM(s.qty * s.price * s.discount/100) AS total_discount
+FROM sales s
+JOIN product_details pd 
+ON s.prod_id = pd.product_id
+GROUP BY pd.category_name;
+
+
+--5. What is the top selling product for each category?
+
+WITH category_sales AS (
+	SELECT 
+		pd.category_name,
+		pd.product_name,
+		SUM(s.qty) AS total_quantity,
+		DENSE_RANK() OVER (PARTITION BY pd.category_name ORDER BY SUM(s.qty) DESC) AS rnk
+	FROM sales s
+	JOIN product_details pd 
+	ON s.prod_id = pd.product_id
+	GROUP BY pd.category_name, pd.product_name
+)
+
+SELECT 
+	category_name,
+	product_name,
+	total_quantity
+FROM category_sales
+WHERE rnk = 1
