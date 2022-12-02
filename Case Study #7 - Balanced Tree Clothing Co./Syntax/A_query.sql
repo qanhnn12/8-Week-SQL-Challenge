d1fd643acf4370fb12e097c971d46edb8d43b7b6
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-02 15:11:06 +0700

    Create A_query.sql

---
+++ Case Study #7 - Balanced Tree Clothing Co./Syntax/A_query.sql
@@ -0,0 +1,20 @@
+--------------------------------
+--A. High Level Sales Analysis--
+--------------------------------
+
+--1. What was the total quantity sold for all products?
+
+SELECT SUM(qty) AS total_quantity
+FROM sales;
+
+
+--2. What is the total generated revenue for all products before discounts?
+
+SELECT SUM(qty * price) AS revenue_before_discounts
+FROM sales;
+
+
+--3. What was the total discount amount for all products?
+
+SELECT SUM(qty * price * discount) AS discount_amount
+FROM sales;
