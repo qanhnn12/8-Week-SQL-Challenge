c25ae3b79d55a8464b127af90fe9d11cdc06fd5e
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-02 16:52:06 +0700

    Update C_query.sql

--- Case Study #7 - Balanced Tree Clothing Co./Syntax/C_query.sql
+++ Case Study #7 - Balanced Tree Clothing Co./Syntax/C_query.sql
@@ -29,7 +29,7 @@ GROUP BY pd.segment_name;
 
 --3. What is the top selling product for each segment?
 
-WITH segment_sales AS (
+WITH segment_product_quantity AS (
 SELECT 
   pd.segment_name,
   pd.product_name,
@@ -45,7 +45,7 @@ SELECT
   segment_name,
   product_name AS top_selling_product,
   total_quantity
-FROM segment_sales
+FROM segment_product_quantity
 WHERE rnk = 1;
 
 
@@ -64,7 +64,7 @@ GROUP BY pd.category_name;
 
 --5. What is the top selling product for each category?
 
-WITH category_sales AS (
+WITH category_product_quantity AS (
   SELECT 
     pd.category_name,
     pd.product_name,
@@ -80,7 +80,69 @@ SELECT
   category_name,
   product_name AS top_selling_product,
   total_quantity
-FROM category_sales
+FROM category_product_quantity
 WHERE rnk = 1;
 
 
+--6. What is the percentage split of revenue by product for each segment?
+
+WITH segment_product_revenue AS (
+	SELECT 
+		pd.segment_name,
+		pd.product_name,
+		SUM(s.qty * s.price) AS product_revenue
+	FROM sales s
+	JOIN product_details pd 
+	ON s.prod_id = pd.product_id
+	GROUP BY pd.segment_name, pd.product_name
+)
+
+SELECT 
+	segment_name,
+	product_name,
+	CAST(100.0 * product_revenue 
+		/ SUM(product_revenue) OVER (PARTITION BY segment_name) 
+		AS decimal (10, 2)) AS segment_product_pct
+FROM segment_product_revenue;
+
+
+--7. What is the percentage split of revenue by segment for each category?
+
+WITH segment_category_revenue AS (
+	SELECT 
+		pd.segment_name,
+		pd.category_name,
+		SUM(s.qty * s.price) AS category_revenue
+	FROM sales s
+	JOIN product_details pd 
+	ON s.prod_id = pd.product_id
+	GROUP BY pd.segment_name, pd.category_name
+)
+
+SELECT 
+	segment_name,
+	category_name,
+	CAST(100.0 * category_revenue 
+		/ SUM(category_revenue) OVER (PARTITION BY category_name) 
+		AS decimal (10, 2)) AS segment_category_pct
+FROM segment_category_revenue;
+
+
+--8. What is the percentage split of total revenue by category?
+
+WITH category_revenue AS (
+	SELECT 
+		pd.category_name,
+		SUM(s.qty * s.price) AS revenue
+	FROM sales s
+	JOIN product_details pd 
+	ON s.prod_id = pd.product_id
+	GROUP BY pd.category_name
+)
+
+SELECT 
+	category_name,
+	CAST(100.0 * revenue 
+		/ SUM(revenue) OVER () 
+		AS decimal (10, 2)) AS category_pct
+FROM category_revenue;
