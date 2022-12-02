f7cd7f747a93c6db2f86b04866b89b705008c77d
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-02 22:11:45 +0700

    Update D_query.sql

--- Case Study #7 - Balanced Tree Clothing Co./Syntax/D_query.sql
+++ Case Study #7 - Balanced Tree Clothing Co./Syntax/D_query.sql
@@ -5,16 +5,19 @@
 --Use a single SQL query to transform the product_hierarchy and product_prices datasets to the product_details table.
 
 SELECT 
-	pp.product_id,
-	pp.price,
-	CONCAT(ph1.level_text, ' ', ph2.level_text, ' - ', ph3.level_text) AS product_name,
-	ph2.parent_id AS category_id,
-	ph1.parent_id AS segment_id,
-	ph1.id AS style_id,
-	ph3.level_text AS category_name,
-	ph2.level_text AS segment_name,
-	ph1.level_text AS style_name
+  pp.product_id,
+  pp.price,
+  CONCAT(ph1.level_text, ' ', ph2.level_text, ' - ', ph3.level_text) AS product_name,
+  ph2.parent_id AS category_id,
+  ph1.parent_id AS segment_id,
+  ph1.id AS style_id,
+  ph3.level_text AS category_name,
+  ph2.level_text AS segment_name,
+  ph1.level_text AS style_name
 FROM product_hierarchy ph1
+--self join style level with segment level
 JOIN product_hierarchy ph2 ON ph1.parent_id = ph2.id
+--self join segment level with category level 
 JOIN product_hierarchy ph3 ON ph3.id = ph2.parent_id
+--inner join style level with table [product_prices] 
 JOIN product_prices pp ON ph1.id = pp.id;
