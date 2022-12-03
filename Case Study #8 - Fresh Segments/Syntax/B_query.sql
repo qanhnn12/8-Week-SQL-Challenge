7c75b34b663cbc13ac964ce029257f71d97615c3
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-03 17:42:19 +0700

    Update B_query.sql

--- Case Study #8 - Fresh Segments/Syntax/B_query.sql
+++ Case Study #8 - Fresh Segments/Syntax/B_query.sql
@@ -6,15 +6,38 @@
 
 -- Find how many unique month_year dates in our dataset
 DECLARE @unique_month_year_cnt INT = (
-	SELECT COUNT(DISTINCT month_year)
-	FROM interest_metrics)
+  SELECT COUNT(DISTINCT month_year)
+  FROM interest_metrics)
 
 --Filter all interest_id that have the count = @unique_month_year_cnt
 SELECT 
-	interest_id,
-	COUNT(month_year) AS cnt
+  interest_id,
+  COUNT(month_year) AS cnt
 FROM interest_metrics
 GROUP BY interest_id
 HAVING COUNT(month_year) = @unique_month_year_cnt;
 
 
+--2. Using this same total_months measure - calculate the cumulative percentage of all records starting at 14 months 
+-- which total_months value passes the 90% cumulative percentage value?
+
+WITH interest_months AS (
+  SELECT
+    interest_id,
+    COUNT(DISTINCT month_year) AS total_months
+  FROM interest_metrics
+  WHERE interest_id IS NOT NULL
+  GROUP BY interest_id
+),
+interest_count AS (
+  SELECT
+    total_months,
+    COUNT(interest_id) AS cnt
+  FROM interest_months
+  GROUP BY total_months
+)
+
+SELECT *,
+  CAST(100.0 * SUM(cnt) OVER(ORDER BY total_months DESC)
+	/ SUM(cnt) OVER() AS decimal(10, 2)) AS cumulative_pct
+FROM interest_count;
