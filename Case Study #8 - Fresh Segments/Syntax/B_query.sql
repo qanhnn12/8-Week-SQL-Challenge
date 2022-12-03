7b2eb2d1764aa795d36b6c8497532106c5cba697
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-03 16:59:19 +0700

    Update B_query.sql

--- Case Study #8 - Fresh Segments/Syntax/B_query.sql
+++ Case Study #8 - Fresh Segments/Syntax/B_query.sql
@@ -3,3 +3,18 @@
 ------------------------
 
 --1. Which interests have been present in all month_year dates in our dataset?
+
+-- Find how many unique month_year dates in our dataset
+DECLARE @unique_month_year_cnt INT = (
+	SELECT COUNT(DISTINCT month_year)
+	FROM interest_metrics)
+
+--Filter all interest_id that have the count = @unique_month_year_cnt
+SELECT 
+	interest_id,
+	COUNT(month_year) AS cnt
+FROM interest_metrics
+GROUP BY interest_id
+HAVING COUNT(month_year) = @unique_month_year_cnt;
+
+
