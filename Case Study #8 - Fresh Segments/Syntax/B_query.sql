fec4d65aa501d762ccbdec17e9c3d37aa6a124c1
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-04 09:15:45 +0700

    Update B_query.sql

--- Case Study #8 - Fresh Segments/Syntax/B_query.sql
+++ Case Study #8 - Fresh Segments/Syntax/B_query.sql
@@ -41,3 +41,27 @@ SELECT *,
   CAST(100.0 * SUM(cnt) OVER(ORDER BY total_months DESC)
 	/ SUM(cnt) OVER() AS decimal(10, 2)) AS cumulative_pct
 FROM interest_count;
+
+
+--3. If we were to remove all interest_id values which are lower than the total_months value we found in the previous question 
+--- how many total data points would we be removing?
+
+WITH interest_months AS (
+  SELECT
+    interest_id,
+    COUNT(DISTINCT month_year) AS total_months
+  FROM interest_metrics
+  WHERE interest_id IS NOT NULL
+  GROUP BY interest_id
+),
+interest_count AS (
+  SELECT
+    total_months,
+    COUNT(interest_id) AS cnt
+  FROM interest_months
+  GROUP BY total_months
+)
+
+SELECT SUM(cnt) AS interest_id_count
+FROM interest_count
+WHERE total_months <= 6;
