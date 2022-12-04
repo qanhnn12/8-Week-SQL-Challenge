7ec73fc1284802a61f8a940333bf634e87e49e46
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-04 15:22:37 +0700

    Create C_query.sql

---
+++ Case Study #8 - Fresh Segments/Syntax/C_query.sql
@@ -0,0 +1,26 @@
+-----------------------
+--C. Segment Analysis--
+-----------------------
+
+--1. Using our filtered dataset by removing the interests with less than 6 months worth of data, 
+--which are the top 10 and bottom 10 interests which have the largest composition values in any month_year? 
+--Only use the maximum composition value for each interest but you must keep the corresponding month_year.
+
+WITH composition_ranks AS (
+	SELECT 
+		month_year,
+		interest_id,
+		composition,
+		MAX(composition) OVER (PARTITION BY month_year) AS largest_composition,
+		DENSE_RANK() OVER(PARTITION BY month_year ORDER BY composition DESC) AS top_rnk,
+		DENSE_RANK() OVER(PARTITION BY month_year ORDER BY composition) AS bottom_rnk
+	FROM interest_metrics
+	WHERE month_year IS NOT NULL
+)
+
+SELECT 
+	DISTINCT cr.interest_id,
+	im.interest_name
+FROM composition_ranks cr
+JOIN interest_map im ON cr.interest_id = im.id
+WHERE cr.bottom_rnk <= 10
