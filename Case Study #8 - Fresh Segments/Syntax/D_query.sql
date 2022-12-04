8d2230b63fad8d46b0ff1434af0689f6835d0ba5
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-04 19:05:10 +0700

    Update D_query.sql

--- Case Study #8 - Fresh Segments/Syntax/D_query.sql
+++ Case Study #8 - Fresh Segments/Syntax/D_query.sql
@@ -52,4 +52,38 @@ FROM top_10_interests
 --4. What is the 3 month rolling average of the max average composition value from September 2018 to August 2019 
 --and include the previous top ranking interests in the same output shown below.
 
+WITH avg_compositions AS (
+	SELECT 
+		month_year,
+		interest_id,
+		ROUND(composition / index_value, 2) AS avg_comp,
+		ROUND(MAX(composition / index_value) 
+		  OVER(PARTITION BY month_year), 2) AS max_avg_comp
+	FROM interest_metrics
+	WHERE month_year IS NOT NULL
+),
+max_avg_compositions AS (
+	SELECT *
+	FROM avg_compositions
+	WHERE avg_comp = max_avg_comp
+),
+moving_avg_compositions AS (
+	SELECT 
+		mac.month_year,
+		im.interest_name,
+		mac.max_avg_comp AS max_index_composition,
+		ROUND(AVG(mac.max_avg_comp) OVER(ORDER BY mac.month_year 
+			ROWS BETWEEN 2 PRECEDING AND CURRENT ROW), 2) AS '3_month_moving_avg',
+		LAG(im.interest_name) OVER (ORDER BY mac.month_year) + ': ' +
+			CAST(LAG(mac.max_avg_comp) OVER (ORDER BY mac.month_year) 
+			AS VARCHAR(4)) AS '1_month_ago',
+		LAG(im.interest_name, 2) OVER (ORDER BY mac.month_year) + ': ' +
+			CAST(LAG(mac.max_avg_comp, 2) OVER (ORDER BY mac.month_year) 
+			AS VARCHAR(4)) AS '2_month_ago'
+	FROM max_avg_compositions mac 
+	JOIN interest_map im ON mac.interest_id = im.id
+)
 
+SELECT *
+FROM moving_avg_compositions
+WHERE month_year BETWEEN '2018-09-01' AND '2019-08-01'
