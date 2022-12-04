e1bc05a4c7ab0df6e4a1bb269c04ca171f6c17d5
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-04 15:35:20 +0700

    Update C_query.sql

--- Case Study #8 - Fresh Segments/Syntax/C_query.sql
+++ Case Study #8 - Fresh Segments/Syntax/C_query.sql
@@ -7,20 +7,30 @@
 --Only use the maximum composition value for each interest but you must keep the corresponding month_year.
 
 WITH composition_ranks AS (
-	SELECT 
-		month_year,
-		interest_id,
-		composition,
-		MAX(composition) OVER (PARTITION BY month_year) AS largest_composition,
-		DENSE_RANK() OVER(PARTITION BY month_year ORDER BY composition DESC) AS top_rnk,
-		DENSE_RANK() OVER(PARTITION BY month_year ORDER BY composition) AS bottom_rnk
-	FROM interest_metrics
-	WHERE month_year IS NOT NULL
+  SELECT 
+    month_year,
+    interest_id,
+    composition,
+    MAX(composition) OVER (PARTITION BY month_year) AS largest_composition,
+    DENSE_RANK() OVER(PARTITION BY month_year ORDER BY composition DESC) AS top_rnk,
+    DENSE_RANK() OVER(PARTITION BY month_year ORDER BY composition) AS bottom_rnk
+  FROM interest_metrics
+  WHERE month_year IS NOT NULL
 )
 
+--Top 10 interests that have the largest composition values in each month_year
 SELECT 
-	DISTINCT cr.interest_id,
-	im.interest_name
+  DISTINCT cr.interest_id,
+  im.interest_name
 FROM composition_ranks cr
 JOIN interest_map im ON cr.interest_id = im.id
-WHERE cr.bottom_rnk <= 10
+WHERE cr.top_rnk <= 10;
+
+--Bottom 10 interests that have the largest composition values in each month_year
+SELECT 
+  DISTINCT cr.interest_id,
+  im.interest_name
+FROM composition_ranks cr
+JOIN interest_map im ON cr.interest_id = im.id
+WHERE cr.bottom_rnk <= 10;
+
