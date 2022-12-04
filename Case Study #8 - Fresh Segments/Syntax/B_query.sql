158bd2099a2bdad81aa5e22d01cab68d10a15e49
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-04 09:46:26 +0700

    Update B_query.sql

--- Case Study #8 - Fresh Segments/Syntax/B_query.sql
+++ Case Study #8 - Fresh Segments/Syntax/B_query.sql
@@ -64,4 +64,23 @@ interest_count AS (
 
 SELECT SUM(cnt) AS interest_id_count
 FROM interest_count
-WHERE total_months <= 6;
+WHERE total_months < 6;
+
+
+--4. 4. Does this decision make sense to remove these data points from a business perspective? 
+--Use an example where there are all 14 months present to a removed interest example for your arguments 
+-- think about what it means to have less months present from a segment perspective.
+
+
+--5. After removing these interests - how many unique interests are there for each month?
+
+--Delete all interest_id that have total_months lower than 6
+DELETE FROM interest_metrics
+WHERE interest_id IN (
+  SELECT
+    interest_id
+  FROM interest_metrics
+  WHERE interest_id IS NOT NULL
+  GROUP BY interest_id
+  HAVING COUNT(DISTINCT month_year) < 6 --total_months in Q3
+  ); 
