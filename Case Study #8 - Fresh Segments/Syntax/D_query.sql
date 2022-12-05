ed3e8850e1706f497b22b5fa5362cd9feced8633
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-05 17:44:25 +0700

    Update D_query.sql

--- Case Study #8 - Fresh Segments/Syntax/D_query.sql
+++ Case Study #8 - Fresh Segments/Syntax/D_query.sql
@@ -88,7 +88,3 @@ moving_avg_compositions AS (
 SELECT *
 FROM moving_avg_compositions
 WHERE month_year BETWEEN '2018-09-01' AND '2019-08-01';
-
-
---5. Provide a possible reason why the max average composition might change from month to month? 
---Could it signal something is not quite right with the overall business model for Fresh Segments?
